3973774a771dc3bf23f1b5c338e1d6e6a9ceae61
Deluxe2186 <yanmife.arayela@gmail.com>
2026-04-28 21:08:40 +0100

    Index buffer

--- Main.cpp
+++ Main.cpp
@@ -17,118 +17,147 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "   FragColor = vec4(0.47f, 0.99f, 0.87f, 1.0f);\n"
 "}\n\0";
 
-int main() 
+
+
+int main()
 {
-	//Initialize GLFW
+	// Initialize GLFW
 	glfwInit();
 
-
-
-	//Tell GLFW what version of openGL we are using
-	//In this case we are using version 3.3
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	//Tell GLFW we are using the CORE profile
-	//so that means we only have te modern functions
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
-	};
-
-	//Create a GLFWwindow object of 800 by 800 pixels, naming it "ClassProject"
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "ClassProject"
 	GLFWwindow* window = glfwCreateWindow(800, 800, "ClassProject", NULL, NULL);
-	//Error check if the window fails to create
+	// Error check if the window fails to create
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	//Introduce the window into the current context
+	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
-
-
 
 	//Load GLAD so it configures OpenGL
 	gladLoadGL();
-	//Specify the viewport of openGL in the Window
-	//In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, 800, 800);
 
+
+
+	// Create Vertex Shader Object and get its reference
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// Attach Vertex Shader source to the Vertex Shader Object
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// Compile the Vertex Shader into machine code
 	glCompileShader(vertexShader);
 
+	// Create Fragment Shader Object and get its reference
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	// Attach Fragment Shader source to the Fragment Shader Object
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	// Compile the Vertex Shader into machine code
 	glCompileShader(fragmentShader);
 
+	// Create Shader Program Object and get its reference
 	GLuint shaderProgram = glCreateProgram();
+	// Attach the Vertex and Fragment Shaders to the Shader Program
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
+	// Wrap-up/Link all the shaders together into the Shader Program
 	glLinkProgram(shaderProgram);
 
+	// Delete the now useless Vertex and Fragment Shader objects
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
 
 
-	
+	// Vertices coordinates
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+		- 0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f
+	};
 
-	GLuint VAO ,VBO;
+	GLuint indices[] =
+	{
+		0, 3, 5,
+		3, 2, 4,
+		5, 4, 1
+	};
 
+	// Create reference containers for the Vartex Array Object and the Vertex Buffer Object
+	GLuint VAO, VBO, EBO;
+
+	// Generate the VAO and VBO with only 1 object each
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
+	// Make the VAO the current Vertex Array Object by binding it
 	glBindVertexArray(VAO);
 
+	// Bind the VBO specifying it's a GL_ARRAY_BUFFER
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Introduce the vertices into the VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Configure the Vertex Attribute so that OpenGL knows how to read the VBO
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Enable the Vertex Attribute so that OpenGL knows to use it
 	glEnableVertexAttribArray(0);
 
+	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
 
-
-
-
-	//Specify the color of the background
-	glClearColor(1.0f, 0.71f, 0.81f, 1.0f);
-	//clean the back buffer and assing the new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
-	//Swap the back buuffer with the front buffer
-	glfwSwapBuffers(window);
-
-
-
-	//Main while loop
+	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Specify the color of the background
 		glClearColor(1.0f, 0.71f, 0.81f, 1.0f);
+		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
 		glUseProgram(shaderProgram);
+		// Bind the VAO so OpenGL knows to use it
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Draw the triangle using the GL_TRIANGLES primitive
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
-		//Take care of all GLFW events
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
+
+
+	// Delete all the objects we've created
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
-
-	//Delete window before ending the program
+	// Delete window before ending the program
 	glfwDestroyWindow(window);
-	//Terminating GLFW before ending the program
+	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
